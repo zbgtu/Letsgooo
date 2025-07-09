33cc65596ab449de6a34b8c17790b209fc9bbe44
zbgtu <jwrondeau33@gmail.com>
2025-07-09 02:33:47 -0400

    Delete path: pages directory

--- path: pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', padding: '4rem' }}>
-      <h1>🚗 Welcome to Letsgooo</h1>
-      <p>VIN engine connected. Calculator, AI, and CRM in progress.</p>
-    </div>
-  );
-}
