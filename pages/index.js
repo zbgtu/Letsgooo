2cbb189c18906a3339cf6d0d0951f13f87c4fa89
zbgtu <jwrondeau33@gmail.com>
2025-07-09 02:36:22 -0400

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>🚗 Welcome to Letsgooo!</h1>
+      <p>This is your first deployed Next.js project on Vercel.</p>
+    </main>
+  );
+}
