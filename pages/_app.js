c8bbceb359db077c110173f7bb72dbbe80075bc2
zbgtu <jwrondeau33@gmail.com>
2025-07-11 13:18:08 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
 import Link from 'next/link';
-import '@/styles/globals.css';
+import '../styles/globals.css'; // ✅ FIXED PATH
 
 export default function App({ Component, pageProps }) {
   return (
